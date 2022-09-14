24619f203a8a67cb4a82eedce3f17a3fdc212640
Perry Werneck <perry.werneck@gmail.com>
2022-09-14 20:58:03 -0300

    Updating debug script.

--- link.sh
+++ link.sh
@@ -4,8 +4,12 @@ if [ "$?" != "0" ]; then
 	exit -1
 fi
 
-sudo ln -sf "$(readlink -f .bin/Debug/udjat-module-sqlite.so)" "/usr/lib64/udjat-modules/1.0"
+MODULEDIR=$(pkg-config --variable module_path libudjat)
+
+mkdir -p ${MODULEDIR}
+sudo ln -sf $(readlink -f .bin/Debug/udjat-module-*.so) ${MODULEDIR}
 if [ "$?" != "0" ]; then
 	exit -1
 fi
+
 
